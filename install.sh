33cab1ff44c49fed39a750a0535797815dcd735b
Getter Yang <getter.y@inwinstack.com>
2018-04-11 15:19:55 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ sudo passwd stack
 echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack
 sudo hostnamectl set-hostname undercloud.tripleo
 sudo hostnamectl set-hostname --transient undercloud.tripleo
-sudo curl -L -o /etc/yum.repos.d/delorean-pike.repo https://trunk.rdoproject.org/centos7-pike/current/delorean.repo
-sudo curl -L -o /etc/yum.repos.d/delorean-deps-pike.repo https://trunk.rdoproject.org/centos7-pike/delorean-deps.repo
-sudo yum install -y yum-plugin-priorities
+tmp=$(curl https://trunk.rdoproject.org/centos7/current/ | grep "python2-tripleo-repos" | awk 'BEGIN {FS="python2-tripleo-repos-"}; {print $2}')
+rpmVersion=$(echo $tmp | awk 'BEGIN {FS=".el7.centos.noarch.rpm"}; {print $1}')
+sudo yum install -y https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-$rpmVersion.el7.centos.noarch.rpm
+sudo -E tripleo-repos -b queens current
 sudo yum install -y python-tripleoclient
 cp /usr/share/instack-undercloud/undercloud.conf.sample ~/undercloud.conf
 sudo yum install -y vim bash-completion vim 
